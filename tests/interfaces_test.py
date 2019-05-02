dde5ea44541ed743f86f98507829651391f13562
Serge Matveenko <lig@countzero.co>
2019-05-02 03:27:21 +0300

    ✅ Test interface inheritance

--- tests/interfaces_test.py
+++ tests/interfaces_test.py
@@ -1,6 +1,18 @@
+import typing
+
 import pytest
 
 import interfaces
+
+
+@pytest.fixture(scope='session')
+def typeT1():
+    return typing.TypeVar('T1')
+
+
+@pytest.fixture(scope='session')
+def typeT2():
+    return typing.TypeVar('T2')
 
 
 def test_010_empty_definition():
@@ -25,34 +37,34 @@ def test_030_error_non_interface_as_implements_value():
 
     with pytest.raises(TypeError):
 
-        class TestClass(interfaces.object, implements=[int]):
+        class TestClass(interfaces.object, implements=[object]):
             pass
 
 
 def test_040_method_is_implemented_no_annotations():
     class TestInterface(interfaces.interface):
-        def method(self, attr):
-            return attr
+        def method(self, arg):
+            pass
 
     class TestClass(interfaces.object, implements=[TestInterface]):
-        def method(self, attr):
-            return attr
+        def method(self, arg):
+            pass
 
 
-def test_050_method_is_implemented_with_annotations():
+def test_050_method_is_implemented_with_annotations(typeT1, typeT2):
     class TestInterface(interfaces.interface):
-        def method(self, attr: str) -> int:
-            return len(attr)
+        def method(self, arg: typeT1) -> typeT2:
+            pass
 
     class TestClass(interfaces.object, implements=[TestInterface]):
-        def method(self, attr: str) -> int:
-            return len(attr)
+        def method(self, arg: typeT1) -> typeT2:
+            pass
 
 
 def test_060_error_method_is_not_implemented():
     class TestInterface(interfaces.interface):
-        def method(self, attr):
-            return attr
+        def method(self, arg):
+            pass
 
     with pytest.raises(interfaces.InterfaceNotImplementedError):
 
@@ -60,53 +72,53 @@ def test_060_error_method_is_not_implemented():
             pass
 
 
-def test_070_error_method_params_signature_is_not_implemented():
+def test_070_error_method_params_signature_is_not_implemented(typeT1, typeT2):
     class TestInterface(interfaces.interface):
-        def method(self, attr: str) -> int:
-            return len(attr)
+        def method(self, arg: typeT1) -> typeT2:
+            pass
 
     with pytest.raises(interfaces.InterfaceNotImplementedError):
 
         class TestClass(interfaces.object, implements=[TestInterface]):
-            def method(self, attr: int) -> int:
-                return attr
+            def method(self, arg: typeT2) -> typeT2:
+                pass
 
 
-def test_080_error_method_return_signature_is_not_implemented():
+def test_080_error_method_return_signature_is_not_implemented(typeT1, typeT2):
     class TestInterface(interfaces.interface):
-        def method(self, attr: str) -> int:
-            return len(attr)
+        def method(self, arg: typeT1) -> typeT2:
+            pass
 
     with pytest.raises(interfaces.InterfaceNotImplementedError):
 
         class TestClass(interfaces.object, implements=[TestInterface]):
-            def method(self, attr: str) -> str:
-                return attr
+            def method(self, arg: typeT1) -> typeT1:
+                pass
 
 
 def test_090_property_is_implemented_no_annotations():
     class TestInterface(interfaces.interface):
         @property
         def value(self):
-            return 42
+            pass
 
     class TestClass(interfaces.object, implements=[TestInterface]):
         @property
         def value(self):
-            return 'sample string'
+            pass
 
 
 def test_100_error_property_is_implemented_as_method_no_annotations():
     class TestInterface(interfaces.interface):
         @property
         def value(self):
-            return 42
+            pass
 
     with pytest.raises(interfaces.InterfaceNotImplementedError):
 
         class TestClass(interfaces.object, implements=[TestInterface]):
             def value(self):
-                return 'sample string'
+                pass
 
 
 def test_110_error_interface_instance():
@@ -115,3 +127,20 @@ def test_110_error_interface_instance():
 
     with pytest.raises(interfaces.InterfaceNoInstanceAllowedError):
         _ = TestInterface()
+
+
+def test_120_interface_inheritance(typeT1, typeT2):
+    class TestInterfaceA(interfaces.interface):
+        def method_a(arg: typeT1) -> typeT1:
+            pass
+
+    class TestInterfaceB(TestInterfaceA):
+        def method_b(arg: typeT2) -> typeT2:
+            pass
+
+    class TestClass(interfaces.object, implements=[TestInterfaceB]):
+        def method_a(arg: typeT1) -> typeT1:
+            pass
+
+        def method_b(arg: typeT2) -> typeT2:
+            pass
