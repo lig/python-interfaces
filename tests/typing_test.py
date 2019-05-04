62cfb85d005c9e9865e77c00f8b688c761ccab1f
Serge Matveenko <lig@countzero.co>
2019-05-05 02:23:32 +0300

    ✅ Add typing annotations test via `mypy`

---
+++ tests/typing_test.py
@@ -0,0 +1,25 @@
+import subprocess
+
+import interfaces
+
+
+def test_typing_self_test() -> None:
+    subprocess.run(f'mypy {__file__}', check=True, shell=True)
+
+
+class SampleInterface(interfaces.interface):  # type: ignore
+    def method(self) -> None:
+        pass
+
+
+class SampleImplementation(
+    interfaces.object, implements=[SampleInterface]  # type: ignore
+):
+    def method(self) -> None:
+        pass
+
+
+def typed_code_wrapper() -> SampleInterface:
+    typed_var: SampleInterface
+    typed_var = SampleInterface()
+    return typed_var
